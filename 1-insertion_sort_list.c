97b8448df19d95fa069d586c2f4313fb958724b2
dairof7 <dairof7@gmail.com>
2020-06-13 17:05:21 -0500

    adding files

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,45 @@
+#include "sort.h"
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *head = *list, *left, *right, *aux1, *aux2;
+
+	if (list == NULL || *list == NULL)
+		return;
+	while (head != NULL)
+	{right = head, left = head->prev, aux1 = head;
+		while (aux1 && aux1->prev)
+		{right = aux1, left = aux1->prev;
+			if (left->n > right->n)
+			{
+				if (right->next == NULL && left->prev == NULL)
+				{
+					left->next = NULL, right->prev = NULL;
+					right->next = left, left->prev = right, *list = right;
+				}
+				else if (right->next == NULL)
+				{
+					left->prev->next = right, right->prev = left->prev;
+					left->next = NULL, right->next = left, left->prev = right;
+				}
+				else if (left->prev == NULL)
+				{
+					right->prev = NULL, left->next = right->next;
+					right->next->prev = left, right->next = left;
+					left->prev = right, *list = right;
+				}
+				else
+				{
+					left->prev->next = right, right->prev = left->prev;
+					left->next = right->next, right->next->prev = left;
+					right->next = left, left->prev = right;
+				}
+				print_list(*list), aux2 = left, left = right, right = aux2;
+			}
+			else
+				break;
+			aux1 = right, aux1 = aux1->prev;
+		}
+		head = aux1, head = head->next;
+	}
+}
