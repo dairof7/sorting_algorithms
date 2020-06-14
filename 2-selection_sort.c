7833f983f505e6fd67952b46a6f431f8ac8d21a0
dairof7 <dairof7@gmail.com>
2020-06-13 19:50:18 -0500

    adding files

---
+++ 2-selection_sort.c
@@ -0,0 +1,29 @@
+#include "sort.h"
+
+void selection_sort(int *array, size_t size)
+{
+	unsigned int i, j;
+	unsigned int min, temp;
+
+	for (i = 0; i < size; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+
+		{
+			if (array[j] < array[min])
+			{
+				min = j;
+			}
+
+		}
+		if (min != i)
+		{
+			temp = array[i];
+			array[i] = array[min];
+			array[min] = temp;
+
+		}
+		print_array(array, size);
+	}
+}
