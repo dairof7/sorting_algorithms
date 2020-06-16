13a1bafad83a0dc47ca4eccfac039bada703dfaf
dairof7 <dairof7@gmail.com>
2020-06-15 20:27:50 -0500

    adding #100

---
+++ 100-shell_sort.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+/**
+ * shell_sort - shell_sort function
+ * @array: the array
+ * @size: the size
+ */
+void shell_sort(int *array, size_t size)
+{
+unsigned int interval = 0, i, idx;
+int val_insert;
+
+	while (interval < size / 3)
+		interval = interval * 3 + 1;
+
+	while (interval > 0)
+	{
+		for (i = interval ; i < size; i++)
+		{
+			val_insert = array[i];
+			idx = i;
+			while (idx > interval - 1 && array[idx - interval] >= val_insert)
+			{
+				array[idx] = array[idx - interval];
+				idx = idx - interval;
+			}
+		array[idx] = val_insert;
+		}
+		interval = (interval - 1) / 3;
+		print_array(array, size);
+	}
+}
--- sort.h
+++ sort.h
@@ -27,5 +27,5 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 int partition(int *array, int low, int high, size_t size);
 void sort(int *array, int low, int high, size_t size);
-
+void shell_sort(int *array, size_t size);
 #endif
